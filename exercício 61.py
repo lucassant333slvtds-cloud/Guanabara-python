95da084886f97666a3b2e261f8ca9325c1ffb1ad
lucassant333slvtds-cloud <lucas.sant333slvtds@gmail.com>
2026-01-22 11:38:32 -0300

    Add files via upload

--- exercício 61.py
+++ exercício 61.py
@@ -1,1 +1,13 @@
-print("Gerador de PA\n-=-=-=-=-=-=-=-=-=-\nPrimeirp termo:")
+print("-="*10)
+print(" |Gerador de PA |")
+print("-="*10)
+pterm = int(input("Qual o primeiro termo?"))
+Rz = int(input("Qual a razão"))
+termo = pterm
+cont = 1
+
+while cont <= 10:
+    print(f"{termo} → ", end=" ")
+    termo += Rz
+    cont += 1
+print("Cabo!")
