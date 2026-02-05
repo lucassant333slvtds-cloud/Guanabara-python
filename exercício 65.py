d2a7c3b01fe85f4e5f8fc5e1181ed2ee87f91d55
lucassant333slvtds-cloud <lucas.sant333slvtds@gmail.com>
2026-02-05 10:35:58 -0300

    Add files via upload

--- exercício 65.py
+++ exercício 65.py
@@ -1,5 +1,14 @@
 resp = "S"
+cont = 0
+soma = 0
+media = 0
+lista = []
 while resp in 'Ss':
     n = int(input("Digite um número: "))
+    lista.append(n)
     resp = str(input("Quer continuar? [S/N]: ")).upper().strip()[0]
-print("cabo!")
+    cont += 1
+    soma += n
+media = soma / cont
+print(f"Você digitou {cont} números e a media foi de {media}")
+print(F"O maior valor foi {max(lista)} e o menor valor foi {min(lista)}")
