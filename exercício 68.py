7b1f558b28efaab69c3202dda33c81de838981a6
Lucas Alves dos Santos <lucas.sant333slvtds@gmail.com>
2026-02-18 11:00:57 -0300

    Add files via upload

--- exercício 68.py
+++ exercício 68.py
@@ -1,41 +1,19 @@
 from random import randint
-from time import sleep
-N_usuario = computador = cont = total = comparador = resultado = 0 
-P_I = " "
-vitoria = 0 
-total = computador + N_usuario
-print("P A R    O U    I M P A R ")
-
-
+N_computador = randint(1,10)
+Tipo = " "
+total = 0
 
 
 while True:
-    N_usuario = int(input("Dgigite um valor entre 1 e 10: "))
-    computador = randint(0,10)
-    total = N_usuario + computador
-    
-    while P_I not in "PI": #par ou impar
-        P_I = str(input("Par ou impar? (P/I) ")).strip()[0]
-    print(f"Vc jogou {N_usuario} e o computaodr jogou {computador}. Total de {total}")
-
-    if P_I == "P":
-        if total % 2 == 0: 
-            print("Acertou!!")
-            vitoria += 1
-                
-        else:
-            print('VOCÊ PERDEU!!!')   
+    print("-=-"*10)
+    print("Vamos jogar Par ou ímpar")
+    print("-=-"*10)
+    Tipo = str(input("Par ou Ímpar? ")).strip().upper()[0]
+    N = int(input("Jogue seu número: "))
+    total = N_computador + N
+    while Tipo == "P":
+        if total % 2 == 0: #par
+        
+            
+        else:# Impar
             break
-
-    elif P_I == "I":
-        if total % 2 == 0: 
-            print('VOCÊ PERDEU!!!')
-            print(f"Pois {total} é PAR!! ")
-            vitoria += 1
-                
-        else:
-            print("Acertou!!")
-            print("Pois {total} é IMPAR")
-            vitoria += 1
-            break
-
